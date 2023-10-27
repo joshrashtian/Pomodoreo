184e2a2a5bb89a4ba0fcbcfad3155a30c536f097
Joshua Rashtian <joshrashtian1@gmail.com>
2023-10-27 16:38:37 -0700

    Working Stopwatch!

--- App.js
+++ App.js
@@ -1,18 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
 export default function App() {
-  const [count, setCount] = useState(0);
+  const [seconds, setSeconds] = useState(0);
+  const [minutes, setMinutes] = useState(0);
+  const [totalseconds, setTotalSeconds] = useState(0);
 
-  const intId = setInterval(() => {
-    setCount(count + 1);
-    clearInterval(intId);
+  useEffect(() => {
+  let interval = setInterval(() => {
+    setTotalSeconds(totalseconds + 1);
+    if(seconds === 59){
+      setMinutes(minutes + 1)
+      setSeconds(0);
+    } else {
+      setSeconds(seconds + 1);
+    }
+    clearInterval(interval);
   }, 1000)
+}, [seconds])
 
+  const displayMinutes = minutes < 10 ? '0' + minutes : minutes;
+  const displaySeconds = seconds < 10 ? '0' + seconds : seconds;
+ 
   return (
     <View style={styles.container}>
-      <Text style={styles.clock}>{count}</Text>
+      <Text>Stopwatch Function</Text>
+      <Text style={styles.clock}>{displayMinutes}:{displaySeconds}</Text>
+      <Text style={{ marginTop: 30}}>Total Seconds Passed: {totalseconds}</Text>
       <StatusBar style="auto" />
     </View>
   );
