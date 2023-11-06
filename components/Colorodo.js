7a83e4f61f088b13742c37a63fcf1a9016cae6cd
Joshua Rashtian <joshrashtian1@gmail.com>
2023-11-05 23:44:26 -0800

    Custom colors

--- components/Colorodo.js
+++ components/Colorodo.js
@@ -1,6 +1,12 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React, { useState } from "react";
-import { ScrollView } from "react-native";
+import { ScrollView, Modal } from "react-native";
 
 export default function Colorodo({ onSumbit }) {
   const [prevcolor, setprevcolor] = useState(null);
@@ -22,6 +28,7 @@ export default function Colorodo({ onSumbit }) {
       Math.floor(Math.random() * 255) +
       ")"
   );
+  const [nummodal, setnummodal] = useState(true);
 
   const handleReturn = () => {
     onSumbit(currentcolor);
@@ -41,17 +48,21 @@ export default function Colorodo({ onSumbit }) {
     );
   };
 
+  const handleColorChange = (color) => {
+    setcurrentcolor("rgb(" + color + ")");
+  };
+
   const lastcolor = () => {
-    setupcomingcolor(currentcolor)
+    setupcomingcolor(currentcolor);
     setcurrentcolor(prevcolor);
     setprevcolor(null);
-  }
+  };
 
   return (
     <View>
       <Text style={styles.title}>Colorodo</Text>
-      <View style={{backgroundColor: '#ffffff'}}>
-      <View
+      <View style={{ backgroundColor: "#ffffff" }}>
+        <View
           style={{
             marginHorizontal: 40,
             marginVertical: 0,
@@ -60,7 +71,7 @@ export default function Colorodo({ onSumbit }) {
             backgroundColor: prevcolor,
           }}
         />
-    </View>
+      </View>
       <View
         style={{
           backgroundColor: "#EEE",
@@ -78,13 +89,22 @@ export default function Colorodo({ onSumbit }) {
           }}
         />
         <Text
-          style={{ marginHorizontal: 20, fontFamily: "Nexa", fontSize: 30, textAlign: 'center' }}
+          style={{
+            marginHorizontal: 20,
+            fontFamily: "Nexa",
+            fontSize: 30,
+            textAlign: "center",
+          }}
         >
-          {currentcolor}
+          {currentcolor === "rgb()"
+            ? "Invalid Color"
+            : currentcolor === "rgb( )"
+            ? "Invalid Color"
+            : currentcolor}
         </Text>
       </View>
-      <View style={{backgroundColor: '#ffffff'}}>
-      <View
+      <View style={{ backgroundColor: "#ffffff" }}>
+        <View
           style={{
             marginHorizontal: 40,
             marginVertical: 10,
@@ -93,15 +113,25 @@ export default function Colorodo({ onSumbit }) {
             backgroundColor: upcomingcolor,
           }}
         />
-    </View>
-      <View style={{ flexDirection: "row", justifyContent: 'space-evenly' }}>
+      </View>
+      <View style={{ flexDirection: "row", justifyContent: "space-evenly" }}>
         <TouchableOpacity onPress={nextColor}>
           <View style={styles.button}>
             <Text style={styles.buttontext}>Next Color</Text>
           </View>
         </TouchableOpacity>
-        <TouchableOpacity onPress={ prevcolor === null ? null : lastcolor}>
-          <View style={ prevcolor === null ? [styles.button, {backgroundColor: '#DDD'}] : styles.button }>
+        <TouchableOpacity
+          onPress={
+            prevcolor === null ? null : prevcolor === "rgb()" ? null : lastcolor
+          }
+        >
+          <View
+            style={
+              prevcolor === null
+                ? [styles.button, { backgroundColor: "#DDD" }]
+                : styles.button
+            }
+          >
             <Text style={styles.buttontext}>Previous Color</Text>
           </View>
         </TouchableOpacity>
@@ -111,6 +141,54 @@ export default function Colorodo({ onSumbit }) {
           </View>
         </TouchableOpacity>
       </View>
+      {nummodal == true ? (
+        <View style={styles.modal}>
+          <Text style={{ fontFamily: "NexaLight", marginBottom: 3 }}>
+            "Colorado Customs"
+          </Text>
+          <View style={styles.inputcontainer}>
+            <Text style={[styles.buttontext, {marginRight: 10}]}>
+              Your Color: 
+            </Text>
+            <TextInput
+              style={styles.textinput}
+              placeholder={"255, 255, 255"}
+              onChangeText={(text) => handleColorChange(text)}
+            />
+          </View>
+          <TouchableOpacity
+            onPress={() => {
+              setnummodal(false);
+            }}
+          >
+            <View>
+              <Text>Minimize</Text>
+            </View>
+          </TouchableOpacity>
+        </View>
+      ) : (
+        <TouchableOpacity
+          onPress={() => {
+            setnummodal(true);
+          }}
+        >
+          <View
+            style={[
+              styles.button,
+              { marginHorizontal: 10, backgroundColor: "#070", marginTop: 10 },
+            ]}
+          >
+            <Text
+              style={[
+                styles.buttontext,
+                { color: "#FFF", fontFamily: "Nexa", textAlign: "center" },
+              ]}
+            >
+              Feeling creative?
+            </Text>
+          </View>
+        </TouchableOpacity>
+      )}
     </View>
   );
 }
@@ -124,11 +202,27 @@ const styles = StyleSheet.create({
   },
   button: {
     padding: 10,
-    backgroundColor: '#eee',
+    backgroundColor: "#eee",
     borderRadius: 30,
   },
   buttontext: {
-    fontFamily: 'NexaLight',
+    fontFamily: "NexaLight",
     fontSize: 16,
+  },
+  modal: {
+    padding: 10,
+    backgroundColor: "#DDD",
+    margin: 10,
+    borderRadius: 20,
+  },
+  textinput: {
+    backgroundColor: "#EEE",
+    padding: 5,
+    borderRadius: 30,
+  },
+  inputcontainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    marginTop: 5,
   }
 });
